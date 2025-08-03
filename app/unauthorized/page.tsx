fbf2a8cbe462ee8599ea245408e1090fc4fbe506
Robbinson <mokorarobbin@gmail.com>
2025-08-03 20:10:59 +0300

    Deleted some fles

--- app/unauthorized/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function UnauthorizedPage() {
-  return (
-    <div className="p-6 text-center text-red-500">
-      <h1 className="text-2xl font-bold">🚫 Unauthorized</h1>
-      <p>You do not have permission to view this page.</p>
-    </div>
-  )
-}
--- lib/auth.ts
+++
@@ -1,5 +0,0 @@
-import jwt from 'jsonwebtoken'
-
-export function verifyToken(token: string) {
-  return jwt.verify(token, process.env.JWT_SECRET!) as { userId: string, role: string }
-}
