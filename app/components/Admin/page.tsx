e96d657a01c46f3f87e3103f4f5c8de56d568b1c
Robbinson <mokorarobbin@gmail.com>
2025-08-05 19:08:41 +0300

    Created routes

---
+++ app/components/Admin/page.tsx
@@ -0,0 +1,16 @@
+import AdminCharts from "./AdminChart";
+import PaymentSection from "./PaymentSection";
+import ProductSection from "./ProductSection";
+
+
+export default function AdminPage() {
+  return (
+    <main className="p-4 space-y-6">
+      <h1 className="text-2xl font-bold">Admin Dashboard</h1>
+      <AdminCharts />
+      <PaymentSection />
+      <ProductSection />
+      
+    </main>
+  )
+}
