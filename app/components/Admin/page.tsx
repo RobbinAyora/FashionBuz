8ccdc5d19552f766b3554d3a4c710cc4334c9b52
Robbinson <mokorarobbin@gmail.com>
2025-08-05 20:06:06 +0300

    Solved errors

--- app/components/Admin/page.tsx
+++ app/components/Admin/page.tsx
@@ -9,7 +9,8 @@ export default function AdminPage() {
   return (
     <main className="p-4 space-y-6">
       <h1 className="text-2xl font-bold">Admin Dashboard</h1>
-      <AdminChart />
+     <AdminChart totalSales={10000} totalOrders={50} pendingOrders={10} />
+
       <PaymentSection />
       <ProductSection />
       
